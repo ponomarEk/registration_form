301d09f434d574004153844b98514d34f180ad14
Oleksandr Ponomarenko <ponomarek91@gmail.com>
2020-06-19 11:43:58 +0300

    Updates

---
+++ precache-manifest.2f50c5a8bce257ad6ed7ca398a6c0be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a38da2ef3ffb56523f40d264de2987d",
+    "url": "/registration_form/index.html"
+  },
+  {
+    "revision": "d4e1c67609cc66ce67c4",
+    "url": "/registration_form/static/css/main.44032eb8.chunk.css"
+  },
+  {
+    "revision": "b0cb518a392065a297ec",
+    "url": "/registration_form/static/js/2.8ea88259.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration_form/static/js/2.8ea88259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e1c67609cc66ce67c4",
+    "url": "/registration_form/static/js/main.87e9dd7a.chunk.js"
+  },
+  {
+    "revision": "16817f9979d4265bfd76",
+    "url": "/registration_form/static/js/runtime-main.b0a71d66.js"
+  },
+  {
+    "revision": "12b7d595678216b8460c28d400eb75bc",
+    "url": "/registration_form/static/media/background.12b7d595.jpg"
+  }
+]);
